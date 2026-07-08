9bad6bcc1632fb3e36ece6dfd43af5a56f67ebcf
ECOSA TECH TEAM <ecosaadmin@gmail.com>
2026-07-08 08:11:07 +0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,1 +1,18 @@
-<h1>Coming soon!</h1>
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div className="card">
+      <h1>Coming Soon!</h1>
+
+      <p>
+        This page is currently under development and will be available soon.
+      </p>
+
+      <p>
+        Thank you for your patience as we continue building the ECOSA Online
+        platform to better serve our alumni community.
+      </p>
+    </div>
+  );
+}
